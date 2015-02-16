698eff27adad159ed9c0a9e4ef901d7d421b388e
Tomas Pilar (tpilar) <tp@tp-desktop.uk.level5networks.com>
2015-02-16 11:36:21 +0000

    Added 256 colors to xterm

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ git submodule init && git submodule update
 
 # Remove all dotfiles from the home directory if present.
 echo Removing any existing dotfiles from your home directory.
-rm -rf ~/.emacs ~/.emacs.d ~/.bashrc ~/.bash_profile ~/.gitconfig ~/.hgrc ~/.config/awesome ~/.dircolors ~/.Xresources
+rm -rf ~/.emacs ~/.emacs.d ~/.bashrc ~/.bash_profile ~/.gitconfig ~/.hgrc ~/.config/awesome ~/.dircolors ~/.Xresources ~/.Xdefaults
 
 # Initialize symlinks.
 echo Creating symlinks in your home directory that point to this dotfiles repository.
@@ -20,6 +20,7 @@ ln -s "$PWD/.dircolors" ~/.dircolors
 [ ! -f /tmp/foo.txt ] && mkdir ~/.config
 ln -s "$PWD/.config/awesome" ~/.config/awesome
 ln -s "$PWD/.Xresources" ~/.Xresources
+ln -s "$PWD/.Xresources" ~/.Xdefaults
 xrdb -m ~/.Xresources
 
 # Finished.
